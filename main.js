945db71d5debddd53b963fc71796792b9bad7c9d
yyj <tulensayyj@gmail.com>
2025-04-02 22:44:08 +0800

    update

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const { app, BrowserWindow } = require('electron')
 
 app.whenReady().then(() => {
-  const win = new BrowserWindow({ width: 800, height: 600 })
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false // 简化示例，生产环境应启用隔离
+    }
+  })
+
   win.loadFile('index.html')
 })
